04c16255544d970689cf3d31ab62539f3157ca48
Saran Jenjirawat <saran@mm.co.th>
2016-10-15 13:00:04 +0700

    Refactor home page view to user a template

--- functional_tests.py
+++ functional_tests.py
@@ -3,11 +3,6 @@ from selenium.webdriver.common.keys import Keys
 import unittest
 import warnings
 
-browser = webdriver.PhantomJS()
-browser.get('http://localhost:8000')
-
-assert 'To-Do' in browser.title, "Browser title was " + browser.title
-
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.PhantomJS()
